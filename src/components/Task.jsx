6788a840e0c7f16931969bfa37c23d1b79a34e80
ankit raj chauhan <ankit@ankits-MacBook-Air.local>
2024-11-16 17:44:24 +0530

    bug fix, was using title as id, biw using id as id for list

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -7,12 +7,12 @@ function Task({ item }) {
   return (
     <li className='p-2 rounded-md bg-gray-100 flex justify-between'>
       <label
-        htmlFor={item.title}
+        htmlFor={item.id}
         className='flex gap-2 items-center cursor-pointer'
       >
         <input
           type='checkbox'
-          id={item.title}
+          id={item.id}
           className='w-4 h-4 border-gray-300 cursor-pointer'
           onChange={() => updateTask(item.id)}
           checked={item.completed}
